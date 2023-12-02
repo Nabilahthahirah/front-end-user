9c56ec12ca201ce85691bf118a159b99da649f75
muti <salsabilamuti@gmail.com>
2023-12-02 07:35:57 +0700

    cart and payment

---
+++ src/app/shopping_cart/page.jsx
@@ -0,0 +1,40 @@
+"use client";
+import Title from "../../components/cart/title.jsx";
+import TableProduct from "@/components/cart/tableProduct.jsx";
+import fotoProduct from "../../components/assets/shoes.jpg";
+import Summary from "@/components/cart/summary.jsx";
+export default function Cart() {
+  const priceProduct = 10000;
+  const totalPrice = 500000;
+  const subTotal = 100000000;
+  return (
+    <div>
+      <Title title="Shopping Cart" />
+      <div className="flex flex- gap-0 mx-20">
+        <div className="basis-3/4">
+          <TableProduct
+            price={priceProduct}
+            nameProduct="Sepatu Anak"
+            sizeProduct="30"
+            colorProduct="gray"
+            photoProduct={fotoProduct}
+          />
+        </div>
+        <div className="basis-1/4">
+          <Summary click="Check Out">
+            <div className="flow-root ">
+              <p className="float-left">Price </p>
+              <p className="float-right">
+                Rp{" "}
+                {subTotal.toLocaleString("id-ID", {
+                  styles: "currency",
+                  currency: "IDR",
+                })}
+              </p>
+            </div>
+          </Summary>
+        </div>
+      </div>
+    </div>
+  );
+}
