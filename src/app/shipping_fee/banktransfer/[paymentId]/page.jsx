1986e296f16405822ab3ccded213b4a6508dc123
muti <salsabilamuti@gmail.com>
2023-12-13 13:21:23 +0700

    update bank transfer

--- src/app/shipping_fee/banktransfer/[paymentId]/page.jsx
+++ src/app/shipping_fee/banktransfer/[paymentId]/page.jsx
@@ -1,7 +1,15 @@
 "use client";
 import fetchData from "@/lib/fetch";
 import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import { useAuthStore } from "@/zustand";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
 export default function BankTransfer({ params }) {
+  const { token, setToken, isLoggedIn, login, logout } = useAuthStore();
+  const router = useRouter();
+  const [imageFile, setImageFile] = useState(null);
+  const [imagePreview, setImagePreview] = useState(null);
   const id = params.paymentId;
   const [totalPayment, setTotalPayment] = useState(0);
   useEffect(() => {
@@ -11,9 +19,6 @@ export default function BankTransfer({ params }) {
           cache: "no-store",
         });
         console.log("respones", response);
-        // const data = await response.data.json();
-        // console.log("data", data);
-        // return;
         setTotalPayment(response.data.total_price);
       } catch (error) {
         console.error("Error fetching payment:", error);
@@ -24,9 +29,51 @@ export default function BankTransfer({ params }) {
   console.log("total payment", totalPayment);
   const url = "/upload/" + id;
 
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setImageFile(file);
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImagePreview(reader.result);
+    };
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+  const upload = async () => {
+    if (!isLoggedIn) {
+      router.push("/auth/login");
+      return;
+    }
+    try {
+      const response = await fetch(`${baseUrl}/api/payment/${id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          product_id: idProduct,
+          quantitiy: quantity,
+        }),
+      });
+      const data = await response?.json();
+      if (response.ok) {
+        toast.success(data.message);
+        router.refresh();
+      } else {
+        toast.error(`${data.error}`);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      toast.error("An error occurred. Please try again later.");
+    }
+  };
+
   return (
-    <div className="flex justify-center p-6 mx-6">
-      <div className="card w-300 bg-base-100 shadow-xl">
+    <div className="flex justify-center p-6 mx-6 ">
+      <div className="card w-200 bg-base-100 shadow-xl">
         <div className="card-body p-10 text-xl">
           <p className="card-title mb-3 justify-center text-2xl">
             Petunjuk Pembayaran
@@ -50,14 +97,65 @@ export default function BankTransfer({ params }) {
             </li>
             <li>
               Simpan bukti transfer sebagai bukti pembayaran dan konfirmasikan
-              pembayaran Anda melalui upload bukti pembayaran{" "}
-              <a href={url} className="link link-primary">
-                disini
-              </a>
-              .
+              pembayaran Anda melalui upload bukti pembayaran dibawah ini.
+              <div className="m-10 max-w-md mx-auto p-6 bg-white border rounded-md shadow-md">
+                <label
+                  htmlFor="file-upload"
+                  className="flex flex-col items-center justify-center w-full h-32 p-4 border-dashed border-2 border-gray-300 rounded-md cursor-pointer"
+                >
+                  <input
+                    required
+                    id="file-upload"
+                    type="file"
+                    className="hidden"
+                    onChange={handleImageChange}
+                  />
+                  {imagePreview ? (
+                    <Image
+                      src={imagePreview}
+                      alt="Selected"
+                      width={500}
+                      height={500}
+                      className="w-32 h-32 object-cover mb-2 rounded-md"
+                    />
+                  ) : (
+                    <div className="flex flex-col items-center">
+                      <svg
+                        className="w-10 h-10 text-gray-400 mb-2"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path d="M12 4v16m8-8H4"></path>
+                      </svg>
+                      <p className="text-sm text-gray-500">
+                        Drag and drop or click to select a file
+                      </p>
+                    </div>
+                  )}
+                </label>
+                {imageFile && (
+                  <div className="mt-4">
+                    <p className="text-lg font-semibold text-gray-700">
+                      {imageFile.name}
+                    </p>
+                  </div>
+                )}
+                <div className="text-center">
+                  <button
+                    type="button"
+                    className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
+                    onClick={upload}
+                  >
+                    Upload
+                  </button>
+                </div>
+              </div>
             </li>
           </ul>
-          <br />
           <p>Catatan penting :</p>
           <ul className="list-decimal">
             <li>
